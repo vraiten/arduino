2b55768d3134ffe422dfe97047134a474cfe0b7c
jp798 <cac.jpkim@gmail.com>
2019-05-06 14:35:46 +0900

    Create led_blick.ino

---
+++ led_blick/led_blick.ino
@@ -0,0 +1,19 @@
+
+#define LED 7 
+
+void setup() {
+
+   pinMode(LED,OUTPUT);
+
+}
+
+void loop() {
+
+  digitalWrite(LED,HIGH);
+  delay(1000);
+
+  digitalWrite(LED,LOW);
+  delay(1000);
+  
+
+}
